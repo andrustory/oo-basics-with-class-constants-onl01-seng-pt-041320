f18e1f9030ef137ca19a07948220dc6449070058
Andres Carvajal <jc1993box@hotmail.com>
2020-05-02 02:02:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand_shoe)
     @brand = brand_shoe
-    BRANDS << brand_shoe if BRANDS.clear(brand)
+    BRANDS << brand_shoe unless BRANDS.clear
   end
   
   def cobble
