91dee3e50c3c1bf813ef74ae2f66651cd78bf8df
Andres Carvajal <jc1993box@hotmail.com>
2020-05-02 01:34:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,6 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-      BRANDS << brand
   end
 
   def cobble
@@ -14,3 +13,28 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 end
+
+def brand=(brand_shoe)
+  @brand = 
+
+#   brands = ["Uggs", "Rainbow"]
+#       brands.each do |brand|
+#         Shoe.new(brand)
+#       end
+
+#       brands.each do |brand|
+#         expect(Shoe::BRANDS).to include(brand)
+#       end
+#     end
+
+#     it 'only keeps track of unique brands' do
+#       Shoe::BRANDS.clear
+#       brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+#       brands.each do |brand|
+#         Shoe.new(brand)
+#       end
+
+#       expect(Shoe::BRANDS.size).to eq(3)
+#     end
+#   end
+# end
